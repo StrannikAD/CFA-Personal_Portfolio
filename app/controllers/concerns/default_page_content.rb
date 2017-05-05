3fa45a6f3972e6c6bed286ad84060f12918be01c
Anna <a.dimitrova.au@gmail.com>
2017-05-05 14:36:45 +1000

    Added custom layouts for blogs and portfolios

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "CFA Portfolio | My Portfolio Website"
     @seo_keywords = "Anna Dimitrova portfolio"
   end
 end
